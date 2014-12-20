9ed7ce6d9699a3dd702da639a70cfe30fd1065d1
Yoskaldyr <yoskaldyr@gmail.com>
2014-12-20 03:18:52 +0200

    Version 1.0.0 + auto update config.php on install/uninstall + early/eager loader feature added. Default state is disabled. To enable CMF_Core_Autoloader::getProxy(true) must be called with parameter (true).

--- upload/library/CMF/Core/Install.php
+++ upload/library/CMF/Core/Install.php
@@ -8,5 +8,66 @@ class CMF_Core_Install
 		{
 		    throw new XenForo_Exception('This Add-On requires XenForo version 1.2.4 or higher.');
 		}
+		/** @var XenForo_Application $app */
+		$app = XenForo_Application::getInstance();
+		$configFile = $app->getConfigDir() . '/config.php';
+		$config = file_get_contents($configFile);
+		$pos = strpos($config, '::getProxy(');
+		if (!$pos)
+		{
+			if (is_writable($configFile))
+			{
+				try
+				{
+					$tempFile = tempnam(XenForo_Helper_File::getTempDir(), 'xf');
+					if ($tempFile)
+					{
+						$config .= "\nCMF_Core_Autoloader::getProxy();";
+						file_put_contents($tempFile, $config);
+						$pos = XenForo_Helper_File::safeRename($tempFile, $configFile);
+					}
+				} catch (Exception $e)
+				{
+				}
+			}
+			if (!$pos)
+			{
+				throw new XenForo_Exception('config.php is not writeable. Please add "CMF_Core_Autoloader::getProxy();" to the end of config.php manually before run install.');
+			}
+		}
+	}
+
+	public static function uninstall()
+	{
+		/** @var XenForo_Application $app */
+		$app = XenForo_Application::getInstance();
+		$configFile = $app->getConfigDir() . '/config.php';
+		$config = file_get_contents($configFile);
+		$pos = strpos($config, '::getProxy(');
+		if ($pos)
+		{
+			if (is_writable($configFile))
+			{
+				try
+				{
+					$tempFile = tempnam(XenForo_Helper_File::getTempDir(), 'xf');
+					if ($tempFile)
+					{
+						$config = preg_replace('#\n[^\n]+::getProxy\([^\n]*\);#u', '', $config);
+						file_put_contents($tempFile, $config);
+						if (XenForo_Helper_File::safeRename($tempFile, $configFile))
+						{
+							$pos = strpos($config, '::getProxy(');
+						}
+					}
+				} catch (Exception $e)
+				{
+				}
+			}
+			if ($pos)
+			{
+				throw new XenForo_Exception('config.php is not writeable. Please remove any "getProxy()" calls from config.php manually before run uninstall.');
+			}
+		}
 	}
 }
